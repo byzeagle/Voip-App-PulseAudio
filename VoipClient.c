748faac943944733d38114819cdd38fc359f28af
Ugur <ugurbd@gmail.com>
2020-07-03 15:32:54 +0300

    Add files via upload

--- VoipClient.c
+++ VoipClient.c
@@ -28,7 +28,13 @@ static const pa_sample_spec ss = {
     .channels = 2
 };
 
-int create_UDP_socket_send(struct sockaddr_in * server)
+void control_s(pa_simple *s)
+{
+    if(s)
+        pa_simple_free(s);
+}
+
+int create_UDP_socket_send(struct sockaddr_in * server, void * address)
 {
     int sock;
     if((sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
@@ -40,9 +46,7 @@ int create_UDP_socket_send(struct sockaddr_in * server)
     memset(server, 0, sizeof(*server));
     server->sin_family = AF_INET;
     server->sin_port = htons(PORT);
-    // server->sin_addr.s_addr = inet_addr("127.0.0.1");
-    // server->sin_addr.s_addr = inet_addr("192.168.0.11");
-    // server->sin_addr.s_addr = inet_addr("192.168.0.14");
+    server->sin_addr.s_addr = inet_addr((const char *)address);
 
     return sock;
 }
@@ -71,20 +75,20 @@ int create_UDP_socket_receive()
     return sock;
 }
 
-void* recorder()
+void* recorder(void * address)
 {
     pa_simple *s = NULL;
     int error;
     int nsent;
 
     struct sockaddr_in server;
-    int socket = create_UDP_socket_send(&server);
+    int socket = create_UDP_socket_send(&server, address);
 
 
     /* Create the recording stream */
     if (!(s = pa_simple_new(NULL, "Voip", PA_STREAM_RECORD, NULL, "record", &ss, NULL, NULL, &error))) {
         fprintf(stderr, __FILE__": pa_simple_new() failed: %s\n", pa_strerror(error));
-        pa_simple_free(s);
+        control_s(s);
         exit(EXIT_FAILURE);
     }
 
@@ -95,14 +99,14 @@ void* recorder()
     	if(pa_simple_read(s, buf, sizeof(buf), &error) < 0)
     	{
     		fprintf(stderr, __FILE__": pa_simple_write() failed: %s\n", pa_strerror(error));
-    		pa_simple_free(s);
+    		control_s(s);
         	exit(EXIT_FAILURE);
     	}
 
-    	if((nsent = sendto(socket, buf, BUFFERSIZE, 0, (struct sockaddr *)&server, sizeof(server))) < 0)
+    	if((nsent = sendto(socket, buf, sizeof(buf), 0, (struct sockaddr *)&server, sizeof(server))) < 0)
 		{
 			perror("sendto()");
-			pa_simple_free(s);
+			control_s(s);
 			exit(EXIT_FAILURE);
 		}
     }
@@ -120,7 +124,7 @@ void* player()
 
     if (!(s = pa_simple_new(NULL, "Voip", PA_STREAM_PLAYBACK, NULL, "playback", &ss, NULL, NULL, &error))) {
         fprintf(stderr, __FILE__": pa_simple_new() failed: %s\n", pa_strerror(error));
-        pa_simple_free(s);
+        control_s(s);
         exit(EXIT_FAILURE);
     }
 
@@ -131,14 +135,14 @@ void* player()
     	if((nread = recvfrom(socket, buf, sizeof(buf), 0, NULL, NULL)) < 0)
 		{
 			perror("recvfrom()");
-            pa_simple_free(s);
+            control_s(s);
 			exit(EXIT_FAILURE);
 		}
 		
 		if(pa_simple_write(s, buf, sizeof(buf), &error) < 0)
 		{
 			fprintf(stderr, __FILE__": pa_simple_write() failed: %s\n", pa_strerror(error));
-			pa_simple_free(s);
+			control_s(s);
         	exit(EXIT_FAILURE);
 		}
     }
@@ -151,13 +155,19 @@ int main(int argc, char * argv[])
     pthread_t thread_send, thread_receive;
 	int rc1, rc2;
 
-    if( (rc1 = pthread_create(&thread_send, NULL, recorder, NULL)))
+    if(argc < 2)
+    {
+        fprintf(stderr, "An ip-address must be provided\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if( (rc1 = pthread_create(&thread_send, NULL, recorder, argv[1])))
     {
         fprintf(stderr, "Thread1 creation failed\n");
         exit(EXIT_FAILURE);
     }
 
-    if( (rc2 = pthread_create(&thread_receive, NULL, player, NULL)))
+    if((rc2 = pthread_create(&thread_receive, NULL, player, NULL)))
     {
         fprintf(stderr, "Thread2 creation failed\n");
         exit(EXIT_FAILURE);
