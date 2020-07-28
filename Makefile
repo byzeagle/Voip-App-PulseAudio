7bee69890bd5167687fe8ae51ab809d3cbc6d00c
Ugur <ugurbd@gmail.com>
2020-07-28 23:53:45 +0300

    Add files via upload

--- Makefile
+++ Makefile
@@ -1,7 +1,12 @@
+CC=gcc
+
 all: VoipClient
 
 VoipClient: VoipClient.c
-	gcc VoipClient.c -o VoipClient -lpulse-simple -lpulse -pthread
+	$(CC) VoipClient.c -o VoipClient -O3 -lpulse-simple -lpulse -pthread -lopus
+
+new: new.c
+	$(CC) -g new.c -o new -lpulse-simple -lpulse -lopus
 
 clean:
-	rm VoipClient
+	rm VoipClient new
--- VoipClient.c
+++ VoipClient.c
@@ -10,21 +10,38 @@
 #include <errno.h>
 #include <pthread.h>
 
-/*
-*   Copyright 2020
-*   Author : Ugur Buyukdurak
-*   Version : 1.0
-*/
-
 #include <pulse/simple.h>
 #include <pulse/error.h>
 
-#define PORT 8080
+#include <opus/opus.h>
+
+#define PORT 11000
+
+/*
+*   Simple Voip Program
+*   Author: Ugur Buyukdurak
+*   Copyright: 2020
+*/
+
 #define BUFFERSIZE 1024
+#define FRAME_SIZE 960
+#define SAMPLE_RATE 48000
+#define CHANNELS 2
+#define MAX_FRAME_SIZE 6 * FRAME_SIZE
+#define MAX_PACKET_SIZE (BUFFERSIZE * 4)
+#define APPLICATION OPUS_APPLICATION_VOIP
+
+struct Data
+{
+    int payload_length;
+    uint8_t cbits[MAX_PACKET_SIZE];
+} __attribute__((__packed__));
+
+typedef struct Data Data;
 
 static const pa_sample_spec ss = {
     .format = PA_SAMPLE_S16LE,
-    .rate = 44100,
+    .rate = 48000,
     .channels = 2
 };
 
@@ -84,32 +101,57 @@ void* recorder(void * address)
     struct sockaddr_in server;
     int socket = create_UDP_socket_send(&server, address);
 
-
-    /* Create the recording stream */
+        /* Create the recording stream */
     if (!(s = pa_simple_new(NULL, "Voip", PA_STREAM_RECORD, NULL, "record", &ss, NULL, NULL, &error))) {
         fprintf(stderr, __FILE__": pa_simple_new() failed: %s\n", pa_strerror(error));
         control_s(s);
         exit(EXIT_FAILURE);
     }
 
-    uint8_t buf[BUFFERSIZE];
+    int err;
+    OpusEncoder *enc;
+
+    enc = opus_encoder_create(SAMPLE_RATE, CHANNELS, APPLICATION, &err);
+    if (err < 0)
+    {
+        fprintf(stderr, "failed to create an encoder: %s\n", opus_strerror(err));
+        exit(EXIT_FAILURE);
+    }
+
+    opus_int16 in[FRAME_SIZE * CHANNELS];
+    uint8_t cbits[MAX_PACKET_SIZE];
+    uint8_t buf[FRAME_SIZE * CHANNELS * 2];
+
+    int nBytes;
+    Data data;
 
     for (;;) 
     {
-    	if(pa_simple_read(s, buf, sizeof(buf), &error) < 0)
-    	{
-    		fprintf(stderr, __FILE__": pa_simple_write() failed: %s\n", pa_strerror(error));
-    		control_s(s);
-        	exit(EXIT_FAILURE);
-    	}
-
-    	if((nsent = sendto(socket, buf, sizeof(buf), 0, (struct sockaddr *)&server, sizeof(server))) < 0)
-		{
-			perror("sendto()");
-			control_s(s);
-			exit(EXIT_FAILURE);
-		}
+     if(pa_simple_read(s, buf, sizeof(buf), &error) < 0)
+     {
+        fprintf(stderr, __FILE__": pa_simple_write() failed: %s\n", pa_strerror(error));
+        control_s(s);
+        exit(EXIT_FAILURE);
     }
+
+    memcpy(in, buf, sizeof(buf));
+    nBytes = opus_encode(enc, in, FRAME_SIZE, cbits, MAX_PACKET_SIZE);
+    if(nBytes < 0)
+    {
+        fprintf(stderr, "Error opus_encode\n");
+        exit(EXIT_FAILURE);
+    }
+
+    data.payload_length = nBytes;
+    memcpy(data.cbits, cbits, sizeof(uint8_t) * nBytes);
+
+    if((nsent = sendto(socket, &data, sizeof(data.payload_length) + nBytes, 0, (struct sockaddr *)&server, sizeof(server))) < 0)
+    {
+        perror("sendto()");
+        control_s(s);
+        exit(EXIT_FAILURE);
+    }
+}
 
     pthread_exit(NULL);
 }
@@ -128,32 +170,61 @@ void* player()
         exit(EXIT_FAILURE);
     }
 
-    uint8_t buf[BUFFERSIZE];
+    int err;
+    OpusDecoder *dec;
+    dec = opus_decoder_create(SAMPLE_RATE, CHANNELS, &err);
+    if (err < 0)
+    {
+        fprintf(stderr, "failed to create an encoder: %s\n", opus_strerror(err));
+        exit(EXIT_FAILURE);
+    }
+
+    opus_int16 out[FRAME_SIZE * CHANNELS];
+    uint8_t cbits[MAX_PACKET_SIZE];
+    uint8_t buf[FRAME_SIZE * CHANNELS * 2];
+
+    int nBytes;
+    Data data;
 
     for (;;) 
     {
-    	if((nread = recvfrom(socket, buf, sizeof(buf), 0, NULL, NULL)) < 0)
-		{
-			perror("recvfrom()");
+        if((nread = recvfrom(socket, &data, sizeof(data), 0, NULL, NULL)) < 0)
+        {
+            perror("recvfrom()");
             control_s(s);
-			exit(EXIT_FAILURE);
-		}
-		
-		if(pa_simple_write(s, buf, sizeof(buf), &error) < 0)
-		{
-			fprintf(stderr, __FILE__": pa_simple_write() failed: %s\n", pa_strerror(error));
-			control_s(s);
-        	exit(EXIT_FAILURE);
-		}
+            exit(EXIT_FAILURE);
+        }
+
+        int payload = data.payload_length;
+        if(nread != sizeof(data.payload_length) + payload){
+            fprintf(stderr, "Error receiving the packet\n");
+            exit(EXIT_FAILURE);
+        }
+
+        nBytes = opus_decode(dec, data.cbits, data.payload_length, out, MAX_FRAME_SIZE, 0);
+        if(nBytes < 0)
+        {
+            fprintf(stderr, "Error opus_decode\n");
+            exit(EXIT_FAILURE);
+        }
+
+        memcpy(buf, out, sizeof(out));
+
+        if(pa_simple_write(s, buf, sizeof(buf), &error) < 0)
+        {
+            fprintf(stderr, __FILE__": pa_simple_write() failed: %s\n", pa_strerror(error));
+            control_s(s);
+            exit(EXIT_FAILURE);
+        }
     }
 
-    pthread_exit(NULL);
+pthread_exit(NULL);
 }
 
 int main(int argc, char * argv[])
 {
     pthread_t thread_send, thread_receive;
-	int rc1, rc2;
+    int rc1, rc2;
 
     if(argc < 2)
     {
@@ -171,10 +242,12 @@ int main(int argc, char * argv[])
     {
         fprintf(stderr, "Thread2 creation failed\n");
         exit(EXIT_FAILURE);
-    }    
+    }
+
+    printf("Port used is: %d\n", PORT);
 
     pthread_join(thread_send, NULL);
     pthread_join(thread_receive, NULL);
 
-	pthread_exit(NULL);
+    pthread_exit(NULL);
 }
